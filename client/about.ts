ec53fab8433db41e2f4063166c83f3cefb8c2798
Methan Sritharan <methanroyal@gmail.com>
2021-12-14 13:00:23 +0000

    Update logo in about.ts

--- client/about.ts
+++ client/about.ts
@@ -10,7 +10,7 @@ export function aboutView(): VNode[] {
     ]
     return [
         h('div.about', [
-            h('img.center', { attrs: { src: `${model["asset-url"]}/favicon/favicon-96x96.png` } }),
+            h('img.center', { attrs: { src: `${model["asset-url"]}/favicon/favicon.png` } }),
             h('h1', { attrs: { align: 'center' } }, _('About Liantichess')),
             h('p', _('Liantichess is a free, open-source antichess server derived from pychess.')),
             h('p', [
